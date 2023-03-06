730d855976d90dc3769aa3a9460b4698aaecacf5
ConnieJacques <connie.jacques91@gmail.com>
2023-03-06 15:10:21 +1100

    models started

---
+++ src/models/movies-directors-production_companies.py
@@ -0,0 +1,25 @@
+from app import db
+
+# Define Movie model
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+    length = db.Column(db.String(), nullable=False)
+    box_office_ranking = db.Column(db.Integer, nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'), nullable=False)
+    production_company_id = db.Column(db.Integer, db.ForeignKey('productioncompany.id'), nullable=False)
+
+# Define Director model
+class Director(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    director_name = db.Column(db.String(), nullable=False)
+    director = db.relationship('Movie', backref='director', cascade="all, delete")
+
+
+# Define ProductionCompany model
+class ProductionCompany(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    production_company = db.relationship('Movie', backref='production_company', cascade="all, delete")
---
+++ src/models/users.py
@@ -0,0 +1,11 @@
+from app import db
+
+# Define User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(), nullable=False)
+    surname = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(8), nullable=False)
+    admin = db.Column(db.Boolean, default=False)
+
