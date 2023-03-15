a327c94cf08955f00e8fa2cf6e1494fa9be2e524
ConnieJacques <connie.jacques91@gmail.com>
2023-03-15 17:29:44 +1100

    move and book schema comments added, order of fields adjusted

--- src/schemas/book_schema.py
+++ src/schemas/book_schema.py
@@ -1,23 +1,22 @@
 from app import ma
 from marshmallow import fields
-from app import db
 
 
 class BookSchema(ma.Schema):
     class Meta:
         ordered = True
         # Define the fields to expose
-        fields = ("id", "title", "isbn", "length", "first_publication_date", "copies_published", "author_id", "publisher_id", "read", "movie", "author", "publisher") 
+        fields = ("id", "title", "isbn", "length", "first_publication_date", "copies_published", "author_id", "publisher_id", "author", "publisher") 
 
-        load_only = ["movie", "read", "author_id", "publisher_id"]
+        # Define fields to load only
+        load_only = ["author_id", "publisher_id"]
 
-    author = ma.Nested("AuthorSchema", only=("published_name", ))
-    publisher = ma.Nested("PublisherSchema", only=("publisher_name", ))
-    read = fields.List(fields.Nested("ReadSchema", only=["rating"]))
-    movie = fields.List(fields.Nested("ReadSchema", only=["rating"]))
+    # # Define fields to expose for nested schema
+    author = ma.Nested("AuthorSchema", only=["published_name"])
+    publisher = ma.Nested("PublisherSchema", only=["publisher_name"])
+    # Format date
     first_publication_date = fields.DateTime(format='%d-%m-%Y')
 
-#add backref to read table and use python control to get average of the books
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
--- src/schemas/movie_schema.py
+++ src/schemas/movie_schema.py
@@ -6,15 +6,18 @@ class MovieSchema(ma.Schema):
     class Meta:
         ordered = True
         # Define the fields to expose
-        fields = ("id", "title", "release_date", "length", "box_office_ranking", "director_id", "production_company_id",  "book_id", "watched", "book", "director", "production") 
+        fields = ("id", "title", "release_date", "length", "box_office_ranking", "director", "production", "book", "director_id", "production_company_id",  "book_id") 
 
-        load_only = ["watched", "book_id", "director_id", "production_company_id", ]
+        # Define fields to load only
+        load_only = ["book_id", "director_id", "production_company_id", ]
 
+    # Define fields to expose for nested schema
     director = ma.Nested("DirectorSchema", only=("director_name", ))
     production = ma.Nested("ProductionSchema", only=("name", ))
     book = ma.Nested("BookSchema", only=("title", ))
-    watched = fields.List(fields.Nested("WatchedSchema", exclude=("watched", )))
+    # Format date
     release_date = fields.DateTime(format='%d-%m-%Y')
+
 
 movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
