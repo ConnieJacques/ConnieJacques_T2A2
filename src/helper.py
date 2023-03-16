d87cdab2b9a1db2cdc14d341de1274fbcb529a49
ConnieJacques <connie.jacques91@gmail.com>
2023-03-16 15:11:43 +1100

    helper comments added

--- src/helper.py
+++ src/helper.py
@@ -4,11 +4,15 @@ from flask import abort
 from functools import wraps
 
 
+# Decorator to handle most errors
 def exception_handler(func):
+    # Wrap function
     @wraps(func)
     def function(*args, **kwargs):
         try:
-            func(*args, **kwargs)
+            # Call and return the function
+            return func(*args, **kwargs)
+        # Return helpful error messages, if function triggers an error
         except exc.NoSuchTableError:
             return abort(404, description="Table not found. Please seed the database.")
         except KeyError:
@@ -16,18 +20,15 @@ def exception_handler(func):
         except exc.NoResultFound:
             return abort(404, "No results found. Please check you are using a valid query method.")
         except exc.DataError:
-            return abort(404, "No results found. Please check you are using a valid query method.")
+            return abort(400, "No results found. Please check you are using a valid query method.")
         except AssertionError:
             return abort(400, description="New entry already exist in the database.")
         # except exceptions.ValidationError:
         #     return abort(400, description="Error in request body. Please check for spelling mistakes. Ensure all required fields are included. Dates must be formatted as DD-MM-YYYY")
         except exc.IntegrityError:
-            return abort(400, description="Data entered in the request body matches an existing entry.")   
+            return abort(400, description="Data entered in the request body matches an existing entry.")
+        except AttributeError:
+            return abort(400, "No results found. Please check your query is accurate.")
+        # except exc.UnprocessableContentError:
+            # return abort(422, message="Invalid bearer token.") 
     return function
-
-
-
-
-
-
-
